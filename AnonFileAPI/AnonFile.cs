600cc98b37d452d22cab69b9ff9d188bbc1402f8
Jobn Blue <Nexus.GraalClassic@aol.com>
2018-02-19 00:39:58 -0500

    Add project files.

---
+++ AnonFileAPI/AnonFile.cs
@@ -0,0 +1,117 @@
+﻿namespace AnonFileAPI
+{
+    public class AnonFile
+    {
+        private string response     = null;
+        private bool status         = false;
+        private string urlfull      = null;
+        private string urlshort     = null;
+        private uint size           = 0;
+        private string errorCode    = null;
+        private string errorType    = null;
+
+        /// <summary>
+        ///     Used to return a successful AnonFile. Should NOT be initialized by user but instead AnonFileWrapper.
+        /// </summary>
+        /// <param name="response"></param>
+        /// <param name="status"></param>
+        /// <param name="urlfull"></param>
+        /// <param name="urlshort"></param>
+        /// <param name="size"></param>
+        public AnonFile(string response, bool status, string urlfull, string urlshort, uint size)
+        {
+            this.response = response;
+            this.status = status;
+            this.urlfull = urlfull;
+            this.urlshort = urlshort;
+            this.size = size;
+        }
+
+        /// <summary>
+        ///     Used to return an error-filled AnonFile. Should NOT be initialized by user but instead AnonFileWrapper.
+        /// </summary>
+        /// <param name="response"></param>
+        /// <param name="status"></param>
+        /// <param name="errorCode"></param>
+        /// <param name="errorType"></param>
+        public AnonFile(string response, bool status, string errorCode, string errorType)
+        {
+            this.response = response;
+            this.status = status;
+            this.errorCode = errorCode;
+            this.errorType = errorType;
+        }
+
+        /// <summary>
+        ///     Return the full URL (basically URL + file name and extension).
+        /// </summary>
+        /// <returns> Full URL. </returns>
+        public string getFullURL()
+        {
+            return urlfull;
+        }
+
+        /// <summary>
+        ///     Return the short URL.
+        /// </summary>
+        /// <returns> Short URL. </returns>
+        public string getShortURL()
+        {
+            return urlshort;
+        }
+
+        /// <summary>
+        ///     Return the amount of bytes in the file. If file upload failed, will return 0.
+        /// </summary>
+        /// <returns> Amount of Bytes. </returns>
+        public uint getAmountOfBytes()
+        {
+            return size;
+        }
+
+        /// <summary>
+        ///     Return the status of the upload.
+        /// </summary>
+        /// <returns> Status. </returns>
+        public bool isGoodResponse()
+        {
+            return status;
+        }
+
+        /// <summary>
+        ///     Return the entire raw JSON.
+        /// </summary>
+        /// <returns> raw JSON. </returns>
+        public string getFullResponse()
+        {
+            return response;
+        }
+
+        /// <summary>
+        ///     Return the error message. If there is no error, this will return null.
+        /// </summary>
+        /// <returns> Error Code. </returns>
+        public string getErrorCode()
+        {
+            return errorCode;
+        }
+
+        /// <summary>
+        ///     Return the error type. If there is no error, this will return null.
+        /// </summary>
+        /// <returns> Error Type. </returns>
+        public string getErrorType()
+        {
+            return errorType;
+        }
+
+        /// <summary>
+        ///     Return the full raw JSON.
+        /// </summary>
+        /// <returns> Raw JSON. </returns>
+        public override string ToString()
+        {
+            return response;
+        }
+    }
+}
---
+++ AnonFileAPI/AnonFileWrapper.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Runtime.Serialization.Json;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using System.IO;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace AnonFileAPI
+{
+    public class AnonFileWrapper : IDisposable
+    {
+        private WebClient client         = null;
+        private string DirectDownloadURL = null;
+        /// <summary>
+        ///     Initializes new WebClient.
+        /// </summary>
+
+        public AnonFileWrapper()
+        {
+            client = new WebClient();
+        }
+
+        /// <summary>
+        ///     Downloads the file to the specified path. 
+        /// </summary>
+        /// <param name="fileURL"> The URL of the file wanted. </param>
+        /// <param name="downloadLocation"> The specified path with file and extension </param>
+        public void DownloadFile(string fileURL, string downloadLocation)
+        {
+           client.DownloadFile(getDirectDownloadLinkFromLink(fileURL), downloadLocation);
+        }
+
+        /// <summary>
+        ///     Checks if file exists and uploads file. This along with parsing the output of the JSON response.
+        /// </summary>
+        /// <param name="fileLocation"> The specified path to the file to be uploaded. </param>
+        public AnonFile UploadFile(string fileLocation)
+        {
+            if (!File.Exists(fileLocation))
+                throw new Exception("Anon File Uploader ERROR: File does not exist!");
+            
+            byte[] uploadValue = client.UploadFile(@"https://anonfile.com/api/upload", fileLocation);
+            return parseOutput(Encoding.Default.GetString(uploadValue));
+        }
+
+        /// <summary>
+        ///     Sorts through the HTML document to find the direct download link (which has a randomly string inserted inside of it). NOT safe threading.
+        /// </summary>
+        /// <param name="htmlDocument"></param>
+        /// <returns></returns>
+        private void unsafeGetDirectDownloadLinkFromLink(string htmlDocument)
+        {
+            using (WebBrowser browser = new WebBrowser())
+            {
+                browser.DocumentText = htmlDocument;
+                browser.ScriptErrorsSuppressed = true;
+                browser.Document.OpenNew(true);
+                browser.Document.Write(htmlDocument);
+                browser.Refresh();
+                DirectDownloadURL = browser.Document.GetElementById("download-url").GetAttribute("href");
+            }
+        }
+
+        /// <summary>
+        ///     Sorts through the link's HTML document to find the direct download link (which has a randomly string inserted inside of it).
+        /// </summary>
+        /// <param name="link"></param>
+        /// <returns></returns>
+        public string getDirectDownloadLinkFromLink(string link)
+        {
+            string HTMLDoc = client.DownloadString(link);
+            Thread threadSafe = new Thread(() => unsafeGetDirectDownloadLinkFromLink(HTMLDoc));
+            threadSafe.SetApartmentState(ApartmentState.STA);
+            threadSafe.Start();
+            threadSafe.Join();
+            return DirectDownloadURL;
+        }
+
+
+        /// <summary>
+        ///     This is the only private method. It parses the JSON reply and returns AnonFiles with set properties. 
+        /// </summary>
+        /// <param name="input"></param>
+        private AnonFile parseOutput(string input)
+        {
+            var jsonReader = JsonReaderWriterFactory.CreateJsonReader(Encoding.UTF8.GetBytes(input), new System.Xml.XmlDictionaryReaderQuotas());
+            var root = XElement.Load(jsonReader);
+            bool status = Convert.ToBoolean(root.XPathSelectElement("//status").Value);
+            if (!status)
+            {
+                string errorCode = root.XPathSelectElement("//error/message").Value;
+                string errorType = root.XPathSelectElement("//error/type").Value;
+                return new AnonFile(input, status, errorCode, errorType);
+            }
+            else
+            {
+                string urlfull = root.XPathSelectElement("//url/full").Value;
+                string urlshort = root.XPathSelectElement("//url/short").Value;
+                uint size = Convert.ToUInt32(root.XPathSelectElement("//metadata/size/bytes").Value);
+                return new AnonFile(input, status, urlfull, urlshort, size);
+            }
+        }
+
+        /// <summary>
+        ///     Dispose the webclient.
+        /// </summary>
+        public void Dispose()
+        {
+            client.Dispose();
+        }
+    }
+}
